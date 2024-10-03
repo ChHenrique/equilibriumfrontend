6c73f3a391cb652acb0af4c22395ca31b8a97cbc
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-10-03 12:54:33 -0300

    a

--- src/pages/search/sections/psicologos.jsx
+++ src/pages/search/sections/psicologos.jsx
@@ -1,6 +1,7 @@
 import { Psicologo } from "../componente_psicologos/info_psicologo1";
 
 import DH from "../../../assets/images/dhamyla_ivina.png"
+import Belinha from "../../../assets/images/isabelle_brito.png"
 
 export function Seach_psicologos() {
     return (
@@ -8,7 +9,7 @@ export function Seach_psicologos() {
 
             <Psicologo nome="Dhamyla Ivina" foto={DH} tempConsulta="50 minutos" formação_psicologo={"KAKAKAKAK"} topico1={"skksks"} topico2={"sososo"}/>
 
-            <Psicologo nome = "Isabelle Brito" foto = {""} tempConsulta= "40 minutos"/>
+            <Psicologo nome = "Isabelle Brito" foto = {Belinha} tempConsulta= "40 minutos"/>
 
             <div></div>
 
